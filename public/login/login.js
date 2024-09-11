4fb8e2cc925ea74d4d884bb8afbc385906e6dfb4
Angel Recio <angel.recio@lendafriend.co>
2024-09-11 00:59:12 -0700

    quitarcomentarios

--- public/login/login.js
+++ public/login/login.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const passwordField = document.getElementById('password');
     const toggleButton = document.getElementById('toggle-password');
-    const eyeOpenedIcon = "../images/eye-opened.png"; // Ruta de la imagen de ojo abierto
-    const eyeClosedIcon = "../images/eye-closed.png"; // Ruta de la imagen de ojo cerrado
-    const toggleIcon = toggleButton.querySelector('img'); // Selecciona la imagen dentro del botón
+    const eyeOpenedIcon = "../images/eye-opened.png"; 
+    const eyeClosedIcon = "../images/eye-closed.png"; 
+    const toggleIcon = toggleButton.querySelector('img'); 
 
     toggleButton.addEventListener('click', function () {
         if (passwordField.type === 'password') {
             passwordField.type = 'text';
-            toggleIcon.src = eyeOpenedIcon; // Cambia a la imagen de ojo abierto
+            toggleIcon.src = eyeOpenedIcon; 
         } else {
             passwordField.type = 'password';
-            toggleIcon.src = eyeClosedIcon; // Cambia a la imagen de ojo cerrado
+            toggleIcon.src = eyeClosedIcon; 
         }
     });
 });
